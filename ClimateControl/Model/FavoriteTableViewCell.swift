5f4d4e62770802fae2a985a51f53cf3c5e362e4e
Brando <bflore11@asu.edu>
2020-11-27 20:29:04 -0700

    Added UITableViewController

---
+++ ClimateControl/Model/FavoriteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteTableViewCell.swift
+//  ClimateControl
+//
+//  Created by Brando Flores on 11/27/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
